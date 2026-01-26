0d7fafe9e13c2c1d8bfc3a0d1a5d61d318587d18
Johnpii1 <okolichimobi08gmail.com>
2026-01-26 17:14:13 +0100

    orignal5

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+  //FOR HEADER BACKGROUND
   const header = document.getElementById("header");
 
   window.addEventListener("scroll", () => {
@@ -6,6 +6,29 @@
       header.classList.add("bg-[#2b0f0f]", "shadow-lg");
     } else {
       header.classList.remove("bg-[#2b0f0f]", "shadow-lg");
+    }
+  });
+
+
+  //FOR MODULAR
+  const openBtn = document.getElementById("openModal");
+  const closeBtn = document.getElementById("closeModal");
+  const modal = document.getElementById("modalOverlay");
+
+  openBtn.addEventListener("click", () => {
+    modal.classList.remove("hidden");
+    modal.classList.add("flex");
+  });
+
+  closeBtn.addEventListener("click", () => {
+    modal.classList.add("hidden");
+    modal.classList.remove("flex");
+  });
+
+  modal.addEventListener("click", (e) => {
+    if (e.target === modal) {
+      modal.classList.add("hidden");
+      modal.classList.remove("flex");
     }
   });
 
